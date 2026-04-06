59fd8ea7ea82e1f43a1b23066da8bb1142a56b20
Adith Suresh <adithsuresh200924@gmail.com>
2026-04-06 15:51:13 +0530

    Update footer link and configure server allowed hosts in Vite

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,7 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      href="https://github.com/Xeven777/valentine"
+      href="https://youtu.be/QDia3e12czc?si=zz4SrqONTQy2Aaaz"
       target="__blank"
     >
       Made with{" "}
